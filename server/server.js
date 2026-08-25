bc5c1db5ed39d3d07c30c50ef86d498cc65f5b5a
Pooja Giri <poojagiribpl@gmail.com>
2026-08-25 12:35:40 +0530

    Backend changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -22,5 +22,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
